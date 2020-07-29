7012cb4cf0ef53a9e66c8e3e99220b8d2c4675dd
mnai01 <imatlak03@gmail.com>
2020-07-28 22:39:45 -0400

    added sidebar functionality for mobile view

--- Portfolio-Frontend/src/constants/links.js
+++ Portfolio-Frontend/src/constants/links.js
@@ -24,26 +24,28 @@ const data = [
   },
 ]
 
-const tempLinks = data.map(link => {
-  return (
-    <li
-      key={link.id}
-      onClick={() => {
-        scrollTo("#" + link.text)
-        console.log("#" + link.text)
-      }}
-    >
-      {/* Change to <Link to={link.url}>{link.text}<Link/> for page navigation */}
-      <a>{link.text}</a>
-    </li>
-  )
-})
+const TempLinks = ({ toggleSidebar }) => {
+  return data.map(link => {
+    return (
+      <li
+        key={link.id}
+        onClick={() => {
+          scrollTo("#" + link.text)
+          toggleSidebar()
+        }}
+      >
+        {/* Change to <Link to={link.url}>{link.text}<Link/> for page navigation */}
+        <a>{link.text}</a>
+      </li>
+    )
+  })
+}
 // I KNOW WE CAN COMBINE IT !!!!!
 
-export default ({ styleClass }) => {
+export default ({ styleClass, toggleSidebar }) => {
   return (
     <ul className={`page-links ${styleClass ? styleClass : ""}`}>
-      {tempLinks}
+      <TempLinks toggleSidebar={toggleSidebar} />
     </ul>
   )
 }
