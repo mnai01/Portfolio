8b77c615f240fcf67217c32b32dc32c2c03a0a90
Ian <imatlak03@gmail.com>
2020-12-31 03:11:56 -0500

    removed htmlAttributes={{ lang: "en" }}

--- Portfolio-Frontend/src/components/SEO.js
+++ Portfolio-Frontend/src/components/SEO.js
@@ -23,7 +23,7 @@ const SEO = ({ title, description }) => {
     // twitterUsername,
   } = site.siteMetadata
   return (
-    <Helmet htmlAttributes={{ lang: "en" }} title={`${title}`}>
+    <Helmet title={`${title}`}>
       <meta name="description" content={description || siteDesc} />
       {/* <meta name="image" content={image} /> */}
       {/* twitter cards */}
